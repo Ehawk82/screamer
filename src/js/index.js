042001f173143b084c60faca3def710b9bd1410f
Jason Graziano <mrehawk82@gmail.com>
2019-01-12 13:57:15 -0500

    sending html to new server

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,17 @@
+var myUI = {
+     
 
-console.log("hello world");
+};
+var http = require("http");
+var fs = require("fs");
+
+fs.readFile('../html/index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }   
+	http.createServer(function (request, response) {
+   		response.writeHead(200, {'Content-Type': 'text/html'});
+   		response.write(html);
+   		response.end();
+	}).listen(8766);
+});
